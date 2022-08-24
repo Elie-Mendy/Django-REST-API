a501bc14414f6658a965a1e946cdd0a628edfd14
Elie-Segula <elie.mendy@segula.fr>
2022-08-24 15:22:57 +0200

    AUT-373 : user model - create superuser

--- app/core/models.py
+++ app/core/models.py
@@ -23,6 +23,20 @@ class UserManager(BaseUserManager):
 
         return user
 
+    # TODO? : normaliser l'email
+    def create_superuser(self, email, password):
+        """Create, save, and return a new superuser."""
+        # if not email:
+        #    raise ValueError('User must have an email adress.')
+        # user = self.model(email=self.normalize_email(email), **extra_fields)
+        # user.set_password(password)
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self.db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
