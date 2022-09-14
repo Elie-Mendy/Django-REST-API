c8be18934fa176f631f20190b8576e1595b1260e
Elie-Mendy <emendy.stud@gmail.com>
2022-09-14 18:02:12 +0200

    AUT-240 : api recipe/tags - implement model

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -21,7 +21,7 @@ from recipe.serializers import (
 RECIPES_URL = reverse('recipe:recipe-list')
 
 def detail_url(recipe_id):
-    """Create and return a recipr detail URL."""
+    """Create and return a recipe detail URL."""
     return reverse('recipe:recipe-detail', args=[recipe_id])
 
 def create_recipe(user, **params):
